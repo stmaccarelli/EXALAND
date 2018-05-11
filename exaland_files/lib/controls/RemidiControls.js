827e7513ac1e21b9957fe1e590009fdde8601384
stmaccarelli <reddo.theking@gmail.com>
2018-05-11 09:24:05 +0200

    fixes and cleanup

--- exaland_files/lib/controls/RemidiControls.js
+++ exaland_files/lib/controls/RemidiControls.js
@@ -6,14 +6,18 @@
 
 THREE.RemidiControls = function ( object, _MIDIInterface) {
 
+	console.log('THREE.RemidiControls init');
+
 	var t = this;
 
 	this.object = object;
 
-	// this.target = new THREE.Vector3( 0, 0, 0 );
+	// this.vec = new THREE.Vector2();
+	this.euler = new THREE.Euler(0,0,0,'YXZ');
+
 
 	this.sensibility = 1;
-	this.smoothing = 0.93;
+	this.smoothing = 0.83;
 
 
 	function log(){
@@ -24,7 +28,7 @@ THREE.RemidiControls = function ( object, _MIDIInterface) {
 
 	this.localData = {}
 
-	midiData = {
+	this.midiData = {
 		pitch: 0,
 		roll: 0,
 		w: false,
@@ -44,7 +48,9 @@ THREE.RemidiControls = function ( object, _MIDIInterface) {
 	_MIDIInterface.registerCallback({
 		midi: [1, 24],
 		callback: function(v){
-				midiData.roll = v / 64 - 1;
+				if( t.midiData.on ){
+					t.midiData.roll = v / 64 - 1;
+				}
 			},
 		context: t
 	});
@@ -53,7 +59,11 @@ THREE.RemidiControls = function ( object, _MIDIInterface) {
 	_MIDIInterface.registerCallback({
 		midi: [1, 22],
 		callback: function(v){
-				midiData.pitch = v / 64 - 1; //Math.min( v, 64 ) / 32 - 1;
+				if( t.midiData.on ){
+					t.midiData.pitch = v / 64 - 1; //Math.min( v, 64 ) / 32 - 1;
+				} else {
+					t.midiData.pitch *= 0.5;
+				}
 			},
 		context: t
 	});
@@ -88,95 +98,16 @@ THREE.RemidiControls = function ( object, _MIDIInterface) {
 	// 	context: t
 	// });
 
+	// on / pause cam CONTROLS
 	_MIDIInterface.registerCallback({
 		midi: [1, 43],
 		callback: function(v){
-					midiData.on = !midiData.on;
-					console.log('midiData.on', midiData.on);
+					t.midiData.on = !t.midiData.on;
+					console.log('t.midiData.on', t.midiData.on);
 		},
 		context: t,
 		isTrigger: true
 	});
-
-
-	var setObjectQuaternion = function () {
-
-		var zee = new THREE.Vector3( 0, 0, 1 );
-
-		var euler = new THREE.Euler();
-
-		var q0 = new THREE.Quaternion();
-
-		var q1 = new THREE.Quaternion( - Math.sqrt( 0.5 ), 0, 0, Math.sqrt( 0.5 ) ); // - PI/2 around the x-axis
-
-		return function ( quaternion, alpha, beta, gamma, orient ) {
-
-			euler.set( beta, alpha, - gamma, 'YXZ' );                       // 'ZXY' for the device, but 'YXZ' for us
-
-			quaternion.setFromEuler( euler );                               // orient the device
-
-			// quaternion.multiply( q1 );                                      // camera looks out the back of the device, not the top
-
-			// quaternion.multiply( q0.setFromAxisAngle( zee, - orient ) );    // adjust for screen orientation
-
-		}
-
-	}();
-
-
-	// this.update = function( ){
-	//
-	// 	// add or lerp values
-  //   for(let key in midiData){
-	// 		if( this.localData[key]===undefined || typeof midiData[key] == 'boolean') this.localData[key] = midiData[key];
-	// 		else {
-	// 			this.localData[key] = THREE.Math.lerp( midiData[key] * Math.abs(midiData[key]), this.localData[key], 0.95);
-	// 		}
-	// 	}
-	//
-	// 	if( this.localData.strafe ){
-	// 		object.translateX( -  this.localData.roll  * this.sensibility * 5.0 );
-	// 		object.translateY(  this.localData.pitch * this.sensibility * 5.0 ); //pitch is hacky
-	// 	}
-	// 	else{
-	//
-	// 		object.translateX(   - this.localData.roll  * this.sensibility * 0.25 );
-	// 		object.translateY(   - this.localData.pitch * this.sensibility * 0.25 ); //pitch is hacky
-	//
-	// 		object.rotateY( - this.localData.roll * this.sensibility);
-	// 		object.rotateX( this.localData.pitch * ( this.sensibility * 0.75) ); //pitch is hacky
-	// 	}
-	//
-	// 	object.translateZ( - this.localData.w * 0.25 + this.localData.s * 0.25);
-	// }
-
-	var vec = new THREE.Vector2();
-	var euler = new THREE.Euler(0,0,0,'YXZ');
-
-
-	this.update = function(){
-
-	  if ( midiData.on ){
-
-			// add or lerp values
-			for(let key in midiData){
-				if( this.localData[key]===undefined || typeof midiData[key] == 'boolean') this.localData[key] = midiData[key];
-				else {
-					this.localData[key] = THREE.Math.lerp( midiData[key] * Math.abs(midiData[key]), this.localData[key], this.smoothing);
-				}
-			}
-
-			euler.x += t.localData.pitch * this.sensibility * 0.012;
-			euler.y -= this.localData.roll * this.sensibility * 0.018;
-			euler.z = -this.localData.roll * this.sensibility * 0.40;
-
-
-
-			object.quaternion.setFromEuler( euler );
-
-		}
-
-	}
 
 
 	// SAFETY KEY COMMANDS
@@ -188,21 +119,43 @@ THREE.RemidiControls = function ( object, _MIDIInterface) {
 			remidiCamOn = !remidiCamOn;
 
 			if(!remidiCamOn){
-				midiData.on = false;
-				euler.set( -.1, 0, 0);
-				object.quaternion.setFromEuler( euler );
+				t.midiData.on = false;
+				t.euler.set( -.1, 0, 0);
+				object.quaternion.setFromEuler( t.euler );
 			}
 			else {
-				midiData.on = true;
+				t.midiData.on = true;
 			}
 		}
 
 	}
-	
+
 	window.addEventListener('keyup', keyup );
 
 
+	this.update = function(){
 
+	  // if ( t.midiData.on ){
+
+			// add or lerp values
+			for(let key in t.midiData){
+				if( t.localData[key]===undefined || typeof t.midiData[key] == 'boolean') t.localData[key] = t.midiData[key];
+				else {
+					t.localData[key] = THREE.Math.lerp( t.midiData[key] * Math.abs( t.midiData[key] ), t.localData[key], t.smoothing );
+				}
+			}
+
+			t.euler.x += t.localData.pitch * t.sensibility * 0.012;
+			t.euler.y -= t.localData.roll * t.sensibility * 0.018;
+			t.euler.z = -t.localData.roll * t.sensibility * 0.40;
+
+
+
+			object.quaternion.setFromEuler( t.euler );
+
+		// }
+
+	}
 
 
 };
